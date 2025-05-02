8aa36ab68ec2ffdeb97f50b539e6bbac06d36266
Zaharkos <veresniuk.pn@ucu.edu.ua>
2025-05-02 12:00:21 +0300

    Create solution.py

---
+++ SortBinaryTreesByLevel/solution.py
@@ -0,0 +1,11 @@
+def tree_iterator(node: Node):
+    nodes = [node]
+    while nodes:
+        yield from nodes
+        for n in nodes[:]:
+            if n.left: nodes.append(n.left)
+            if n.right: nodes.append(n.right)
+            nodes.remove(n)
+
+def tree_by_levels(node):
+    return [n.value for n in tree_iterator(node)] if node else []
